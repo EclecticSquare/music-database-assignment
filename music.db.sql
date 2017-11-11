9c7c635fb5b6897a78333a6b0dac85af565c67d7
Tina Bryson <tinabryson@Tinas-MacBook-Pro.local>
2017-11-11 09:20:20 -0500

    all tables added and code corrected

--- music.db.sql
+++ music.db.sql
@@ -1,28 +1,28 @@
-Create Table Songwriter (
+CREATE TABLE Songwriter (
   id SERIAL UNIQUE,
   name varchar(30)
-)
+);
 
 Create Table Song (
   id SERIAL UNIQUE,
   name varchar(30),
   songwriter_id integer
-)
+);
 
-Create Table Track {
+Create Table Track (
   id SERIAL UNIQUE,
   song_id integer,
   song_name varchar(30),
   album_id integer
-}
+);
 
-Create Table Album {
+Create Table Album (
   id SERIAL UNIQUE,
   name varchar(30),
   artist_id integer
-}
+);
 
-Create Table Artist {
+Create Table Artist (
   id SERIAL UNIQUE,
   name varchar(30)
-}
+);
